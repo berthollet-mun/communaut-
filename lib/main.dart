e1c1f3e3262f77558c8bcb75e437a5f4b509d11f
berthollet-mun <bertholletmunihire@gmail.com>
2026-03-08 02:02:28 +0200

    style: optimisation des contrastes en mode sombre et mode clair

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:community/app/initialization.dart';
 import 'package:community/app/routes/app_pages.dart';
 import 'package:community/app/routes/app_routes.dart';
 import 'package:community/app/themes/app_theme.dart';
+import 'package:community/controllers/theme_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:get/get.dart';
@@ -29,44 +30,48 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'Community',
-      debugShowCheckedModeBanner: false,
+    final themeController = Get.find<ThemeController>();
 
-      // Thèmes
-      themeMode: ThemeMode.light,
-      theme: AppTheme.lightTheme,
-      darkTheme: AppTheme.darkTheme,
+    return Obx(
+      () => GetMaterialApp(
+        title: 'Community',
+        debugShowCheckedModeBanner: false,
 
-      // Routes
-      initialRoute: AppRoutes.splash,
-      getPages: AppPages.pages,
+        // Thèmes
+        themeMode: themeController.currentTheme.value,
+        theme: AppTheme.lightTheme,
+        darkTheme: AppTheme.darkTheme,
 
-      // Localisation
-      localizationsDelegates: const [
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: const [Locale('fr', 'FR')],
-      locale: const Locale('fr', 'FR'),
+        // Routes
+        initialRoute: AppRoutes.splash,
+        getPages: AppPages.pages,
 
-      // Transitions
-      defaultTransition: Transition.cupertino,
-      transitionDuration: const Duration(milliseconds: 300),
+        // Localisation
+        localizationsDelegates: const [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: const [Locale('fr', 'FR')],
+        locale: const Locale('fr', 'FR'),
 
-      // Configuration des logs
-      enableLog: true,
-      logWriterCallback: (text, {bool isError = false}) {
-        if (isError) {
-          print('🔴 GETX ERROR: $text');
-        } else {
-          print('🔵 GETX INFO: $text');
-        }
-      },
+        // Transitions
+        defaultTransition: Transition.cupertino,
+        transitionDuration: const Duration(milliseconds: 300),
 
-      // Désactiver le back gesture sur Android
-      popGesture: false,
+        // Configuration des logs
+        enableLog: true,
+        logWriterCallback: (text, {bool isError = false}) {
+          if (isError) {
+            print('🔴 GETX ERROR: $text');
+          } else {
+            print('🔵 GETX INFO: $text');
+          }
+        },
+
+        // Désactiver le back gesture sur Android
+        popGesture: false,
+      ),
     );
   }
 }
